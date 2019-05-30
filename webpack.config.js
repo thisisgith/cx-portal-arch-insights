e0da85f9777a0bc1bd56e07ecb4e376578abb6e8
thayduk2 <thayduk2@cisco.com>
2019-05-30 08:24:11 -0400

    Fixing pipline errors from linting and conflicting webpack versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    resolve: {
-      extensions: ['.ts', '.js'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          exclude: [/node_modules/],
-          loader: 'ts-loader',
-        },
-      ],
-    },
-  }
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: [/node_modules/],
+				loader: 'ts-loader',
+			},
+		],
+	},
+};
