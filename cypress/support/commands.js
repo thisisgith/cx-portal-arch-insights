08a612e2e3909a802857b6e7a215ddd84264ce1e
mirober2 <mirober2@cisco.com>
2019-06-10 12:16:00 -0700

    PBC-151: Test automation for Asset 360 modal

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,14 @@
 /**
 * Launches the app under test
 */
-Cypress.Commands.add('loadApp', () => {
-	cy.visit('/pbc/');
+Cypress.Commands.add('loadApp', (path = '/') => {
+	cy.visit(`pbc${path}`);
 });
 
+/**
+ * Waits for the app to fully load
+ * @param {number} timeout Time in milliseconds to wait
+ */
 Cypress.Commands.add('waitForAppLoading', (timeout = 30000) => {
 	cy.window({ timeout }).should('have.property', 'loading', false);
 });
