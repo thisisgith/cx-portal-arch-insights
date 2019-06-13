580f1cf4d5dd74d6ef1149e9554b75a56d5dabdb
jakparks <jakparks@cisco.com>
2019-06-13 11:49:40 -0400

    Fixed linting errors.

--- src/app/components/search/general-search/general-search.component.ts
+++ src/app/components/search/general-search/general-search.component.ts
@@ -21,7 +21,6 @@ type RefreshType = 'query' | 'filters' | 'newPage';
  */
 @Component({
 	selector: 'app-general-search',
-	styleUrls: ['./general-search.component.scss'],
 	templateUrl: './general-search.component.html',
 })
 export class GeneralSearchComponent implements OnInit, OnDestroy, OnChanges {
