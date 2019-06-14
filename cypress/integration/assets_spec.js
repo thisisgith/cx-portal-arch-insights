e0095c01834f9ce2b4362b2de3e4d1827f2e539b
mirober2 <mirober2@cisco.com>
2019-06-14 12:01:27 -0400

    PBC-158: Test automation for Asset 360 Activity tab and timeline

--- cypress/integration/assets_spec.js
+++ cypress/integration/assets_spec.js
@@ -5,6 +5,16 @@ const assetMock = new MockService('HardwareScenarios');
 const hardwareScenario = assetMock.getScenario('GET', 'Hardware');
 const assets = hardwareScenario.response.body.data;
 
+// TODO: Add data-auto-ids to cui-tab, then remove this mapping
+const tabMap = {
+	Details: 0,
+	Hardware: 1,
+	Software: 2,
+	Vulnerabilities: 3,
+	Insights: 4,
+	Activity: 5,
+};
+
 describe('Assets', () => { // PBC-41
 	before(() => {
 		cy.login();
@@ -29,5 +39,16 @@ describe('Assets', () => { // PBC-41
 		cy.get('tr').eq(3).click(); // switch to new asset without closing modal
 		cy.getByAutoId('Asset360SerialNumber').should('have.text', `SN: ${assets[2].serialNumber}`);
 		cy.getByAutoId('ClearAsset').click();
+	});
+
+	it('Provides an Activity timeline in the 360 view modal', () => {
+		cy.get('tr').eq(1).click();
+		cy.get('asset-details div.tab__heading').eq(tabMap.Activity).click();
+
+		// TODO: This is all placeholder data to be replaced when the API is ready
+		cy.get('div.timeline__time').eq(0).should('have.text', 'a few seconds ago');
+		cy.get('div.timeline__content').eq(0)
+			.should('contain', 'Title')
+			.and('contain', 'Lorem ipsum');
 	});
 });
