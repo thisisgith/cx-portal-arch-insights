1c1016dd5fa0687f7401cc4a7ae6c8b945b555b2
mirober2 <mirober2@cisco.com>
2019-06-27 15:08:30 -0400

    WIP: Asset list tests

--- cypress/integration/assets_spec.js
+++ cypress/integration/assets_spec.js
@@ -65,4 +65,63 @@ describe('Assets', () => { // PBC-41
 			// (*) waiting on https://gitlab-sjc.cisco.com/sso-apps/libraries/cui-x-staging/merge_requests/13
 		});
 	});
+
+	context.only('PBC-36: Asset List', () => {
+		it('Displays assets correctly in list view', () => {
+			cy.get('tbody > tr').should('have.length', assets.length);
+			Cypress._.each(assets, asset => {
+				const serial = asset.serialNumber;
+				cy.getByAutoId(`InventoryItem-${serial}`).within(() => {
+					// TODO: Asset device icon exists after PBC-257
+					cy.getByAutoId(`Device-${serial}`).should('have.text', asset.deviceName);
+					cy.getByAutoId(`IP Address-${serial}`).should('have.text', asset.ipAddress);
+					// TODO: "Last Scan" is not implemented yet
+					cy.getByAutoId(`Last Scan-${serial}`).should('have.text', 'Never');
+					// TODO: "Critical Advisories" is not implemented yet
+					// cy.getByAutoId(`Critical Advisories-${serial}`).should('have.text', '');
+					if (asset.supportCovered) {
+						cy.getByAutoId('SupportCoveredIcon').should('be.visible');
+					} else {
+						cy.getByAutoId('SupportCoveredIcon').should('not.be.visible');
+					}
+					cy.getByAutoId(`Serial Number-${serial}`).should('have.text', asset.serialNumber);
+					cy.getByAutoId(`Software Type-${serial}`).should('have.text', asset.osType);
+					cy.getByAutoId(`Software Version-${serial}`).should('have.text', asset.osVersion);
+					if (asset.role) {
+						cy.getByAutoId(`Role-${serial}`).should('have.text', asset.role);
+					}
+				});
+			});
+		});
+
+		it('Supports selecting one or more assets in the list', () => {
+			cy.get('tbody > tr').eq(0).click() // click row
+				.should('have.class', 'active');
+			cy.getByAutoId(`InventoryItemSelect-${assets[1].serialNumber}`).click(); // click checkbox
+			cy.get('tbody > tr').eq(2).should('not.have.class', 'active');
+			cy.getByAutoId('TotalSelectedCount').should('have.text', '2 Selected');
+			cy.get('tbody > tr').eq(0).click()
+				.should('not.have.class', 'active');
+			cy.getByAutoId('AllAssetSelect').click();
+			cy.getByAutoId('TotalSelectedCount').should('have.text', `${assets.length} Selected`);
+			cy.getByAutoId('AllAssetSelect').click();
+			cy.getByAutoId('TotalSelectedCount').should('not.exist');
+		});
+
+		it('Renders table gracefully when APIs are unavailable', () => {
+			// TODO: Needs mock service changes
+		});
+
+		it('Filters asset list with all visual filters', () => {
+
+		});
+
+		it.skip('Renders visual filtes gracefully when APIs are unavailable', () => {
+			// TODO: PBC-254
+		});
+
+		it('Combines visual filters appropriately', () => {
+
+		});
+	});
 });
