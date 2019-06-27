884141a2960c8d22326a3b147ab3b52c170ef5d1
Gordon Fedoriw <fedoriw@cisco.com>
2019-06-27 09:30:12 -0400

    commit before resync with develop

--- cypress/integration/general_spec.js
+++ cypress/integration/general_spec.js
@@ -21,10 +21,10 @@ describe('General Spec', () => {
 			cy.waitForAppLoading();
 		});
 
-		it.skip('Search and close', () => {
-			const rmaVal = '639530286';
+		it.skip('General Search and close', () => {
+			const searchVal = '639530286';
 			cy.getByAutoId('searchBarInput').should('exist').clear()
-				.type(rmaVal.concat('{enter}'));
+				.type(searchVal.concat('{enter}'));
 			cy.wait(1000);
 			cy.getByAutoId('searchSiteSelect').should('exist');
 			cy.getByAutoId('cui-select').should('exist');			// 2 found
@@ -67,7 +67,9 @@ describe('General Spec', () => {
 			cy.waitForAppLoading();
 		});
 
-		it('RMA 800000000', () => {
+		it('RMA 800000000 one replacement parts', () => {
+			// RMA with 1 replacement part PBC-171 
+			// mock set at "RMA with one replacement part"
 			const rmaVal = '800000000';
 			cy.getByAutoId('searchBarInput').should('exist').clear()
 				.type(rmaVal.concat('{enter}'));
@@ -79,9 +81,10 @@ describe('General Spec', () => {
 			cy.get('app-rma-search table th').eq(2).should('have.text', 'Carrier Tracking Number');
 			cy.get('app-rma-search table th').eq(3).should('have.text', 'Contract Number');
 			cy.get('app-rma-search table th').eq(4).should('have.text', 'Replacement Product');
+			// TODO confirm replacement product contains one product string
 			cy.get('app-rma-search table th').eq(5).should('have.text', 'Replacement Product ID');
 			cy.getByAutoId('RMAViewDetailsButton').should('exist').click();
-			// cy.get('h5').should('contain', 'Related to this Product');
+			cy.get('h5').should('contain', 'Related to this Product');
 			// cy.get('app-no-results').should('contain', 'Suggestions');
 			// cy.get('app-no-results').should('contain', 'Check your spelling');
 			// cy.get('app-no-results').should('contain', 'Try different keyword');
@@ -96,6 +99,67 @@ describe('General Spec', () => {
 			cy.getByAutoId('searchClose').should('exist').click();
 		});
 
+		it('RMA 800000000 four replacement parts', () => {
+			// RMA with 4 replacement part PBC-171 
+			// mock set at "RMA with four replacement parts"
+			const rmaVal = '800000000';
+			cy.getByAutoId('searchBarInput').should('exist').clear()
+				.type(rmaVal.concat('{enter}'));
+			cy.wait(4000);
+			cy.get('app-rma-search').should('contain', 'RMA: '.concat(rmaVal));
+			cy.get('app-rma-search table th').eq(0).should('have.text', 'Status');
+			cy.get('app-rma-search table th').eq(1).should('have.text', 'Case Number');
+			cy.get('app-rma-search table th').eq(2).should('have.text', 'Carrier Tracking Number');
+			cy.get('app-rma-search table th').eq(3).should('have.text', 'Contract Number');
+			cy.get('app-rma-search table th').eq(4).should('have.text', 'Replacement Product');
+			// TODO confirm replacement product contains four product strings
+			cy.get('app-rma-search table th').eq(5).should('have.text', 'Replacement Product ID');
+			cy.getByAutoId('RMAViewDetailsButton').should('exist').click();
+			cy.get('h5').should('contain', 'Related to this Product');
+			// cy.get('app-no-results').should('contain', 'Suggestions');
+			// cy.get('app-no-results').should('contain', 'Check your spelling');
+			// cy.get('app-no-results').should('contain', 'Try different keyword');
+			// cy.get('app-no-results').should('contain', 'Try more general keywords');
+			// cy.get('app-no-results').should('contain', 'Did you know you can search on');
+			// cy.get('app-no-results').should('contain', 'Serial Number');
+			// cy.get('app-no-results').should('contain', 'Case Number');
+			// cy.get('app-no-results').should('contain', 'Contract Number');
+			// cy.get('app-no-results').should('contain', 'RMA Number');
+			// eslint-disable-next-line max-len
+			// cy.get('app-no-results').should('contain', 'Information about that item will be displayed if you are entitled to view it.');
+			cy.getByAutoId('searchClose').should('exist').click();
+		});
+
+		it('RMA 800000000 no replacement parts', () => {
+			// RMA with 4 replacement part PBC-171 
+			// mock set at "RMA with no replacement parts"
+			const rmaVal = '800000000';
+			cy.getByAutoId('searchBarInput').should('exist').clear()
+				.type(rmaVal.concat('{enter}'));
+			cy.wait(4000);
+			cy.get('app-rma-search').should('contain', 'RMA: '.concat(rmaVal));
+			cy.get('app-rma-search table th').eq(0).should('have.text', 'Status');
+			cy.get('app-rma-search table th').eq(1).should('have.text', 'Case Number');
+			cy.get('app-rma-search table th').eq(2).should('have.text', 'Carrier Tracking Number');
+			cy.get('app-rma-search table th').eq(3).should('have.text', 'Contract Number');
+			cy.get('app-rma-search table th').eq(4).should('have.text', 'Replacement Product');
+			// TODO confirm replacement product contains "N/A"
+			cy.get('app-rma-search table th').eq(5).should('have.text', 'Replacement Product ID');
+			cy.getByAutoId('RMAViewDetailsButton').should('exist').click();
+			cy.get('h5').should('contain', 'Related to this Product');
+			// cy.get('app-no-results').should('contain', 'Suggestions');
+			// cy.get('app-no-results').should('contain', 'Check your spelling');
+			// cy.get('app-no-results').should('contain', 'Try different keyword');
+			// cy.get('app-no-results').should('contain', 'Try more general keywords');
+			// cy.get('app-no-results').should('contain', 'Did you know you can search on');
+			// cy.get('app-no-results').should('contain', 'Serial Number');
+			// cy.get('app-no-results').should('contain', 'Case Number');
+			// cy.get('app-no-results').should('contain', 'Contract Number');
+			// cy.get('app-no-results').should('contain', 'RMA Number');
+			// eslint-disable-next-line max-len
+			// cy.get('app-no-results').should('contain', 'Information about that item will be displayed if you are entitled to view it.');
+			cy.getByAutoId('searchClose').should('exist').click();
+		});
 		it('RMA 800000009', () => {
 			const inputVal = '800000009';
 			cy.getByAutoId('searchBarInput').should('exist').clear()
