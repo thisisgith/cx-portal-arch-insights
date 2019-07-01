955f07e39e0368bc3fbd2a5c5a6111d6d17d0f4c
Carl Lindner <clindner@cisco.com>
2019-07-01 14:12:18 -0400

    Added tie environment for PBC

---
+++ src/environments/environment.tie.ts
@@ -0,0 +1,24 @@
+import { defaults } from './defaults';
+import { mockSettings } from './mock/mock';
+
+/**
+ * Stage Internal origin
+ */
+const origin = 'https://tie.cisco.com';
+
+/**
+ * Contains configurations for internal stage builds, will extend the default configuration
+ * and currently the mock configuration
+ */
+export const environment = {
+	...defaults,
+	...mockSettings,
+	origin,
+	auth: {
+		ciscoTokenUrl: `${origin}/ws/oauth/v3/token/cisco`,
+		referUrl: `${origin}/ws/oauth/v3/sso/`,
+		tokenUrl: `${origin}/ws/oauth/v3/token/cway/35ae50e6ff5843f59183da19cc44245d`,
+	},
+	production: true,
+	sdpOrigin: 'https://api-stage.cisco.com',
+};
