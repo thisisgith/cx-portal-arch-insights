14683e765b4ec5234bd5e0259b882f40810e5c69
sruv <sruv@cisco.com>
2019-07-09 17:05:21 +0530

    PBC-231 case list view implementation

--- src/app/pages/solution/resolution/resolution.module.ts
+++ src/app/pages/solution/resolution/resolution.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
+<<<<<<< HEAD
 import { FormsModule } from '@angular/forms';
+=======
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+>>>>>>> PBC-231 case list view implementation
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ResolutionComponent } from './resolution.component';
 import { Panel360Module } from '@components';
+<<<<<<< HEAD
+=======
+
+import { CuiTableModule, CuiPagerModule, CuiSpinnerModule } from '@cisco-ngx/cui-components';
+import { I18nPipeModule } from '@cisco-ngx/cui-pipes';
+>>>>>>> PBC-231 case list view implementation
 
 /**
  * Child routes for Assets Module for lazy loading
@@ -25,6 +35,11 @@ const childRoutes: Routes = [
 		FormsModule,
 		Panel360Module,
 		RouterModule.forChild(childRoutes),
+		ReactiveFormsModule,
+		CuiPagerModule,
+		CuiTableModule,
+		CuiSpinnerModule,
+		I18nPipeModule,
 	],
 })
 export class ResolutionModule { }
