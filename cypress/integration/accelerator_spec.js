a56e1dd4677c4824194a84ccc4eabd7597b41a9d
thayduk2 <thayduk2@cisco.com>
2019-07-10 09:44:36 -0400

    Removing stray .only from ACC Cypress spec

--- cypress/integration/accelerator_spec.js
+++ cypress/integration/accelerator_spec.js
@@ -27,7 +27,7 @@ describe('Accelerator (ACC)', () => { // PBC-32
 		// No other data-auto-id's exist at this time
 	});
 
-	it.only('ACC tile has a view all link to display ACCs in card view', () => { // PBC-159
+	it('ACC tile has a view all link to display ACCs in card view', () => { // PBC-159
 		cy.getByAutoId('ShowModalPanel-_Accelerator_').click();
 		cy.get('.modal__header.acc__header').should('contain', i18n._Accelerator_)
 			.and('contain', i18n._1on1Coaching_);
