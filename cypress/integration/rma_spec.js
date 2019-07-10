b914c299e9a06de5e845c7c0d7f25efff0c69867
Gordon Fedoriw <fedoriw@cisco.com>
2019-07-10 15:41:54 -0400

    refactoring rma due to pbc219

--- cypress/integration/rma_spec.js
+++ cypress/integration/rma_spec.js
@@ -1,5 +1,7 @@
 
 describe('RMA Spec', () => {
+	const i18n = require('../../src/assets/i18n/en-US.json');
+
 	context('RMA Search', () => {
 		before(() => {
 			cy.login();
@@ -7,8 +9,7 @@ describe('RMA Spec', () => {
 			cy.waitForAppLoading();
 		});
 
-		// TODO: Unskip and refactor test once PBC-219 is merged
-		it.skip('RMA 800000000 one replacement parts', () => {
+		it('RMA 800000000 one replacement parts', () => {
 			// RMA with 1 replacement part PBC-171
 			// mock set at "RMA with one replacement part"
 			const rmaVal = '800000000';
@@ -16,22 +17,27 @@ describe('RMA Spec', () => {
 			cy.route('**/esps/search/suggest/cdcpr01zad?*').as('rma');
 			cy.getByAutoId('searchBarInput').should('exist').clear()
 				.type(rmaVal.concat('{enter}'));
+
 			cy.wait('@rma').then(() => {
-				cy.get('app-rma-search').should('contain', 'RMA: '.concat(rmaVal));
-				cy.get('app-rma-search table th').eq(0).should('have.text', 'Status');
-				cy.get('app-rma-search table th').eq(1).should('have.text', 'Case Number');
-				cy.get('app-rma-search table th').eq(2).should('have.text', 'Carrier Tracking Number');
-				cy.get('app-rma-search table th').eq(3).should('have.text', 'Contract Number');
-				// Confirm replacement product contains one product string
-				cy.get('app-rma-search table').within(() => {
-					cy.get('th').eq(4).should('have.text', 'Replacement Product');
-					cy.get('td').should('contain', '^Cisco ASR 920-12SZ-IM Router 0');
-				});
-				cy.get('app-rma-search table th').eq(5).should('have.text', 'Replacement Product ID');
-				cy.getByAutoId('RMAViewDetailsButton').should('exist').click();
-				cy.get('h6').should('contain', 'Related to this Product');
-				cy.getByAutoId('searchClose').should('exist').click();
+				cy.getByAutoId('rmaStatus').should('exist');
+				cy.getByAutoId('rmaNumber').should('exist');
+				cy.getByAutoId('caseNumber').should('exist');
+				cy.getByAutoId('rmaTrackingNumber').should('exist');
+				cy.getByAutoId('contractNumber').should('exist');
+				cy.getByAutoId('rmaViewDetButton').should('exist');
+				cy.getByAutoId('rmaProduct').should('exist');
+				cy.getByAutoId('rmaProdID').should('exist');
+				cy.getByAutoId('rmaProdSeries').should('exist');
+				cy.getByAutoId('rmaEOSaleD').should('exist');
+				cy.getByAutoId('rmaEOSupportD').should('exist');
+				cy.getByAutoId('rmaViewDetButton').should('exist');
 			});
+			// Search
+			// cy.getByAutoId('searchHeader').should('exist').should('contain', i18n.header);
+			cy.getByAutoId('searchHeader').should('exist');
+			cy.getByAutoId('filterBy').should('exist');
+			cy.getByAutoId('relGenRes').should('exist');
+			cy.getByAutoId('searchClose').should('exist').click();
 		});
 
 		// TODO: Unskip and refactor test once PBC-219 is merged
@@ -102,7 +108,7 @@ describe('RMA Spec', () => {
 			});
 		});
 
-		it('RMA 800000009', () => {
+		it.skip('RMA 800000009', () => {
 			const inputVal = '800000009';
 			cy.server();
 			cy.route('**/esps/search/suggest/cdcpr01zad?*').as('rma');
