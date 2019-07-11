600056e3c279d27d2bc9eb30b2ad01201d13f032
Gordon Fedoriw <fedoriw@cisco.com>
2019-07-11 11:07:07 -0400

    refactor general to contain rma and case

--- cypress/integration/general_spec.js
+++ cypress/integration/general_spec.js
@@ -1,18 +1,19 @@
 import MockService from '../support/mockService';
 
-// const searchMock = new MockService('SearchScenarios');
-// import { mockSettings } from '../../src/environments/mock/mock';
+const i18n = require('../../src/assets/i18n/en-US.json');
 
 describe('General Spec', () => {
-	it('Loads the app', () => {
-		cy.loadApp();
-		cy.get('h1.page-title').should('have.text', 'CX Console');
-	});
+	context('Basic Loading Sanity', () => {
+		it('Loads the app', () => {
+			cy.loadApp();
+			cy.get('h1.page-title').should('have.text', 'CX Console');
+		});
 
-	it('Requires SSO login', () => {
-		MockService.mockUnauthenticatedUser();
-		cy.loadApp();
-		cy.url().should('contain', 'https://cloudsso-test.cisco.com/idp/SSO.saml2');
+		it('Requires SSO login', () => {
+			MockService.mockUnauthenticatedUser();
+			cy.loadApp();
+			cy.url().should('contain', 'https://cloudsso-test.cisco.com/idp/SSO.saml2');
+		});
 	});
 
 	context('General Search', () => {
@@ -73,6 +74,217 @@ describe('General Spec', () => {
 				cy.get('app-no-results').should('contain', 'Contract Number');
 				cy.get('app-no-results').should('contain', 'RMA Number');
 				cy.get('app-no-results').should('contain', 'Information about that item will be displayed if you are entitled to view it.');
+				cy.getByAutoId('searchClose').should('exist').click();
+			});
+		});
+	});
+
+	context('Case Search', () => {
+		before(() => {
+			cy.login();
+			cy.loadApp();
+			cy.waitForAppLoading();
+		});
+
+		it('Case Search', () => {
+			// PBC-169
+			const caseVal = '688296392'; // '686569178' '688296392' also works
+			cy.server();
+			cy.route('**/esps/search/suggest/cdcpr01zad?*').as('case');
+			cy.getByAutoId('searchBarInput').should('exist').clear()
+				.type(caseVal.concat('{enter}'));
+			cy.wait('@case').then(() => {
+				cy.getByAutoId('caseNum').should('exist').should('contain', ('Case ').concat(caseVal));
+				cy.getByAutoId('latestUpdate').should('exist').should('contain', i18n._LatestUpdate_);
+				cy.getByAutoId('caseStatus').should('exist').should('contain', i18n._Status_);
+				cy.getByAutoId('caseOwner').should('exist').should('contain', i18n._CaseOwner_);
+				cy.getByAutoId('caseSeverity').should('exist').should('contain', i18n._Severity_);
+				cy.getByAutoId('caseCreated').should('exist').should('contain', i18n._Created_);
+				cy.getByAutoId('caseContract').should('exist').should('contain', i18n._Contract_);
+				cy.getByAutoId('caseDevice').should('exist').should('contain', i18n._Device_);
+				cy.getByAutoId('caseTACEng').should('exist').should('contain', i18n._TACEngineer_);
+				cy.getByAutoId('caseTracking').should('exist').should('contain', i18n._TrackingNumber_);
+				cy.getByAutoId('caseRelRMAs').should('exist').should('contain', i18n._RelatedRMAs_);
+				cy.getByAutoId('viewCaseDetailsB').should('exist').should('contain', i18n._ViewCaseDetails_);
+				cy.getByAutoId('viewAllOpenCasesB').should('exist').should('contain', i18n._ViewAllOpenCases_);
+			});
+			cy.getByAutoId('searchSiteSelect').should('exist');
+			cy.getByAutoId('searchTypeSelect').should('exist');
+			cy.getByAutoId('cui-select')
+				.should($cuiselect => {
+					expect($cuiselect).to.have.length(2);
+				});
+			cy.getByAutoId('searchResultLinkPre0').should('exist');
+			cy.getByAutoId('searchResultLinkPre1').should('exist');
+			cy.getByAutoId('searchResultLinkPre2').should('exist');
+			cy.getByAutoId('searchClose').should('exist').click();
+		});
+
+		it('Case not found', () => {
+			// PBC-169
+			const caseVal = '686568888';
+			cy.server();
+			cy.route('**/esps/search/suggest/cdcpr01zad?*').as('case');
+			cy.getByAutoId('searchBarInput').should('exist').clear()
+				.type(caseVal.concat('{enter}'));
+			cy.wait('@case').then(() => {
+				cy.getByAutoId('caseNum').should('not.exist'); // .should('contain', i18n._Case_);
+				cy.get('app-general-search').should('contain', '10 Results for "'.concat(caseVal).concat('"'));
+				cy.getByAutoId('searchSiteSelect').should('exist');
+				cy.getByAutoId('searchTypeSelect').should('exist');
+				cy.getByAutoId('cui-select')
+					.should($cuiselect => {
+						expect($cuiselect).to.have.length(2);
+					});
+				cy.getByAutoId('searchResultLinkPre0').should('exist');
+				cy.getByAutoId('searchResultLinkPre1').should('exist');
+				cy.getByAutoId('searchResultLinkPre2').should('exist');
+				cy.getByAutoId('searchClose').should('exist').click();
+			});
+		});
+	});
+	context('RMA Search', () => {
+		before(() => {
+			cy.login();
+			cy.loadApp();
+			cy.waitForAppLoading();
+		});
+
+		it('RMA 800000000 one replacement parts', () => {
+			// RMA with 1 replacement part PBC-171
+			// mock set at "RMA with one replacement part"
+			const rmaVal = '800000000';
+			cy.server();
+			cy.route('**/esps/search/suggest/cdcpr01zad?*').as('rma');
+			cy.getByAutoId('searchBarInput').should('exist').clear()
+				.type(rmaVal.concat('{enter}'));
+
+			cy.wait('@rma').then(() => {
+				cy.getByAutoId('rmaStatus').should('exist').should('contain', i18n._Status_);
+				cy.getByAutoId('rmaNumber').should('exist');
+				cy.getByAutoId('caseNumber').should('exist');
+				cy.getByAutoId('rmaTrackingNumber').should('exist');
+				cy.getByAutoId('contractNumber').should('exist');
+				cy.getByAutoId('rmaViewDetButton').should('exist');
+				cy.getByAutoId('rmaProduct').should('exist');
+				cy.getByAutoId('rmaProdID').should('exist');
+				cy.getByAutoId('rmaProdSeries').should('exist');
+				cy.getByAutoId('rmaEOSaleD').should('exist');
+				cy.getByAutoId('rmaEOSupportD').should('exist');
+				cy.getByAutoId('rmaViewDetButton').should('exist');
+			});
+			// General Search section
+			cy.getByAutoId('searchHeader').should('exist');
+			cy.getByAutoId('filterBy').should('exist');
+			cy.getByAutoId('cui-select')
+				.should($cuiselect => {
+					expect($cuiselect).to.have.length(2);
+				}); // Unavailable 2 times
+			cy.getByAutoId('relGenRes').should('exist');
+			cy.getByAutoId('searchClose').should('exist').click();
+		});
+
+		it('RMA 800000000 four replacement parts', () => {
+			// RMA with 4 replacement part PBC-171
+			// mock set at "RMA with four replacement parts"
+			cy.window().then(win => {
+				// cy.log(win.mockService.getEnabledScenarios());
+				win.mockService.enable('RMA with four replacement parts'); // enable the desired
+				cy.getByAutoId('Facet-Assets & Coverage').should('exist').click(); // refresh after making a mock change
+			});
+			// cy.log("one");
+			const rmaVal = '800000000';
+			cy.server();
+			cy.route('**/esps/search/suggest/cdcpr01zad?*').as('rma');
+			cy.getByAutoId('searchBarInput').should('exist').clear()
+				.type(rmaVal.concat('{enter}'));
+			cy.wait('@rma').then(() => {
+				cy.getByAutoId('rmaStatus').should('exist').should('contain', i18n._Status_);
+				cy.getByAutoId('rmaNumber').should('exist');
+				cy.getByAutoId('caseNumber').should('exist');
+				cy.getByAutoId('rmaProductNames').should('exist');
+				cy.getByAutoId('rmaProductIDs').should('exist');
+				// Confirm replacement product contains four product rows
+				cy.getByAutoId('rmaProdDescr')
+					.should($rmaProdDescr => {
+						expect($rmaProdDescr).to.have.length(4);
+					});
+				cy.getByAutoId('rmaProdID')
+					.should($rmaProdID => {
+						expect($rmaProdID).to.have.length(4);
+					});
+				cy.getByAutoId('rmaViewDetButton').should('exist');
+				// General Search section
+				cy.getByAutoId('searchHeader').should('exist');
+				cy.getByAutoId('filterBy').should('exist');
+				cy.getByAutoId('cui-select')
+					.should($cuiselect => {
+						expect($cuiselect).to.have.length(2);
+					});
+				cy.getByAutoId('relGenRes').should('exist');
+				cy.getByAutoId('searchClose').should('exist').click();
+			});
+		});
+
+		it('RMA 800000000 no replacement parts', () => {
+			// RMA with no replacement part PBC-171
+			// mock set at "RMA with no replacement parts"
+			cy.window().then(win => {
+				// cy.log(win.mockService.getEnabledScenarios());
+				win.mockService.enable('RMA with no replacement parts'); // enable the desired
+				cy.getByAutoId('Facet-Assets & Coverage').should('exist').click(); // refresh after making a mock change
+			});
+
+			const rmaVal = '800000000';
+			cy.server();
+			cy.route('**/esps/search/suggest/cdcpr01zad?*').as('rma');
+			cy.getByAutoId('searchBarInput').should('exist').clear()
+				.type(rmaVal.concat('{enter}'));
+			cy.wait('@rma').then(() => {
+				cy.getByAutoId('rmaStatus').should('exist').should('contain', i18n._Status_);
+				cy.getByAutoId('rmaNumber').should('exist');
+				cy.getByAutoId('caseNumber').should('exist');
+				cy.getByAutoId('rmaTrackingNumber').should('exist');
+				cy.getByAutoId('contractNumber').should('exist');
+				cy.getByAutoId('rmaViewDetButton').should('exist');
+				cy.getByAutoId('rmaProduct').should('exist');
+				cy.getByAutoId('rmaUnavail')
+					.should($rmaUnavail => {
+						expect($rmaUnavail).to.have.length(5);
+					});
+				cy.getByAutoId('rmaProdID').should('exist');
+				cy.getByAutoId('rmaProdSeries').should('exist');
+				cy.getByAutoId('rmaEOSaleD').should('exist');
+				cy.getByAutoId('rmaEOSupportD').should('exist');
+				cy.getByAutoId('rmaViewDetButton').should('exist');
+				// General Search section
+				cy.getByAutoId('searchHeader').should('exist');
+				cy.getByAutoId('filterBy').should('exist');
+				cy.getByAutoId('cui-select')
+					.should($cuiselect => {
+						expect($cuiselect).to.have.length(2);
+					});
+				cy.getByAutoId('relGenRes').should('exist');
+				cy.getByAutoId('searchClose').should('exist').click();
+			});
+		});
+
+		it('RMA Not Found 800000009', () => {
+			const inputVal = '800000009';
+			cy.server();
+			cy.route('**/esps/search/suggest/cdcpr01zad?*').as('rma');
+			cy.getByAutoId('searchBarInput').should('exist').clear()
+				.type(inputVal.concat('{enter}'));
+			cy.wait('@rma').then(() => {
+				cy.get('app-general-search').should('contain', '10 Results for "'.concat(inputVal).concat('"'));
+				cy.getByAutoId('searchSiteSelect').should('exist');
+				cy.getByAutoId('cui-select')
+					.should($cuiselect => {
+						expect($cuiselect).to.have.length(2);
+					});
+				cy.getByAutoId('searchResultLinkPre0').should('exist');
+				cy.getByAutoId('searchResultLinkPre1').should('exist');
+				cy.getByAutoId('searchResultLinkPre2').should('exist');
 				cy.getByAutoId('searchClose').should('exist').click();
 			});
 		});
