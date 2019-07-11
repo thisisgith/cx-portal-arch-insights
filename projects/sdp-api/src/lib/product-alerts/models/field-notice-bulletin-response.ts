c1ce19115712d43946a070f7083a5f3db13b140d
Josh Slaughter <joslaugh@cisco.com>
2019-07-11 12:42:48 -0700

    Assets Track Sprint 2

--- projects/sdp-api/src/lib/product-alerts/models/field-notice-bulletin-response.ts
+++ projects/sdp-api/src/lib/product-alerts/models/field-notice-bulletin-response.ts
@@ -1,5 +1,7 @@
 /* tslint:disable */
 import { FieldNoticeBulletin } from './field-notice-bulletin';
+import { Pagination } from './pagination';
 export interface FieldNoticeBulletinResponse {
   data: Array<FieldNoticeBulletin>;
+  Pagination?: Pagination;
 }
