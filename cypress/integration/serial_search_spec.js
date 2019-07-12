33baa870d62ac49c09fd81d112a2050322fd28d2
Gordon Fedoriw <fedoriw@cisco.com>
2019-07-12 10:01:36 -0400

    Update serial search as per PBC-219

--- cypress/integration/serial_search_spec.js
+++ cypress/integration/serial_search_spec.js
@@ -10,20 +10,19 @@ describe('Serial Spec', () => {
 			cy.waitForAppLoading();
 		});
 
-		it.skip('Serial Search FOX1306GFKH', () => {
+		it('Serial Search FOX1306GFKH', () => {
 			// PBC-170
 			const serialVal = 'FOX1306GFKH';
 			cy.server();
-			cy.route('**/esps/search/suggest/cdcpr01zad?*').as('serial'); // TODO might need to update route
+			cy.route('**/esps/search/suggest/cdcpr01zad?*').as('serial');
 			cy.getByAutoId('searchBarInput').should('exist').clear()
 				.type(serialVal.concat('{enter}'));
 			cy.wait('@serial').then(() => {
-				cy.getByAutoId('serialConNum').should('exist');
-				cy.getByAutoId('softwareTypeOS').should('exist');
-				cy.getByAutoId('currentVersion').should('exist');
+				cy.getByAutoId('serialHeader').should('exist');
 				cy.getByAutoId('serialProd').should('exist').should('contain', i18n._Product_);
 				cy.getByAutoId('serialCoverage').should('exist').should('contain', i18n._SupportCoverage_);
 				cy.getByAutoId('serialContract').should('exist').should('contain', i18n._Contract_);
+				cy.getByAutoId('serialConNum').should('exist');
 				cy.getByAutoId('serialExpDate').should('exist').should('contain', i18n._ExpirationDate_);
 				cy.getByAutoId('serialSWTypeOS').should('exist').should('contain', i18n._SoftwareTypeOS_);
 				cy.getByAutoId('serialCurrentVer').should('exist').should('contain', i18n._CurrentVersion_);
@@ -32,12 +31,13 @@ describe('Serial Spec', () => {
 				cy.getByAutoId('serialFieldNotice').should('exist').should('contain', i18n._RelatedFieldNotices_);
 				cy.getByAutoId('serialSecAdv').should('exist').should('contain', i18n._RelatedSecurityAdvisories_);
 				cy.getByAutoId('serialBugs').should('exist').should('contain', i18n._RelatedBugs_);
-				cy.getByAutoId('serialSecAdv').should('exist').should('contain', i18n._RelatedSecurityAdvisories_);
-				cy.getByAutoId('serialSecAdv').should('exist').should('contain', i18n._RelatedSecurityAdvisories_);
-				cy.getByAutoId('serialSecAdv').should('exist').should('contain', i18n._RelatedSecurityAdvisories_);
+				cy.getByAutoId('serialProdID').should('exist').should('contain', i18n._ProductID_);
+				cy.getByAutoId('serialProdSeries').should('exist').should('contain', i18n._ProductSeries_);
+				cy.getByAutoId('serialIP').should('exist').should('contain', i18n._IPAddress_);
+				cy.getByAutoId('serialHost').should('exist').should('contain', i18n._HostName_);
+				cy.getByAutoId('viewDeviceButton').should('exist').should('contain', i18n._ViewDeviceDetails_);
+				cy.getByAutoId('openCaseButton').should('exist').should('contain', i18n._OpenACase_);
 			});
-			cy.getByAutoId('viewDeviceButton').should('exist');
-			cy.getByAutoId('openCaseButton').should('exist');
 
 			// General Search section
 			cy.getByAutoId('searchHeader').should('exist');
@@ -45,13 +45,12 @@ describe('Serial Spec', () => {
 			cy.getByAutoId('cui-select')
 				.should($cuiselect => {
 					expect($cuiselect).to.have.length(2);
-				}); // Unavailable 2 times
+				}); // 2 times
 			cy.getByAutoId('relGenRes').should('exist');
 			cy.getByAutoId('searchClose').should('exist').click();
 		});
 
-		// TODO: Unskip and fix once PBC-219 is merged
-		it.skip('Serial not found FOX1306GFJJ', () => {
+		it('Serial search not found FOX1306GFJJ', () => {
 			// PBC-170
 			const serialVal = 'FOX1306GFJJ';
 			cy.server();
@@ -59,11 +58,14 @@ describe('Serial Spec', () => {
 			cy.getByAutoId('searchBarInput').should('exist').clear()
 				.type(serialVal.concat('{enter}'));
 			cy.wait('@serial').then(() => {
-				cy.get('h3').should('not.exist', 'Serial Number '.concat(serialVal));
+				cy.getByAutoId('serialHeader').should('not.exist');
 				cy.get('app-general-search').should('contain', '10 Results for "'.concat(serialVal).concat('"'));
 				cy.getByAutoId('searchSiteSelect').should('exist');
 				cy.getByAutoId('searchTypeSelect').should('exist');
-				cy.getByAutoId('cui-select').should('exist');			// 2 found
+				cy.getByAutoId('cui-select')
+					.should($cuiselect => {
+						expect($cuiselect).to.have.length(2);
+					}); // 2 times
 				cy.getByAutoId('searchResultLinkPre0').should('exist');
 				cy.getByAutoId('searchResultLinkPre1').should('exist');
 				cy.getByAutoId('searchResultLinkPre2').should('exist');
