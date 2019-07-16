0e830232ed19f5e8046fcaccbb0eb78d05fdaafb
Gordon Fedoriw <fedoriw@cisco.com>
2019-07-16 10:55:08 -0400

    WIP push case detail

--- cypress/integration/casedetail_spec.js
+++ cypress/integration/casedetail_spec.js
@@ -11,10 +11,11 @@ describe('Case Detail Spec', () => {
 
 		it('Case Details ', () => {
 			// PBC-TBD
-			// const serialVal = 'FOX1306GFKH';
 			cy.getByAutoId('Facet-Problem Resolution').should('exist').click(); // refresh after making a mock change
 			cy.server();
 			cy.route('**/esps/search/suggest/cdcpr01zad?*').as('serial');
+			cy.getByAutoId('OPEN CASESTab').should('exist');
+			cy.getByAutoId('RMASTab').should('exist');
 		});
 	});
 });
