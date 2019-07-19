c4a620066ece552b299137eb0ce8fc4e85a45056
Zachery Thomas (zacthoma) <zacthoma@cisco.com>
2019-07-19 15:21:45 -0400

    Update policy page to show api results

--- src/app/pages/admin/policies/policies.component.ts
+++ src/app/pages/admin/policies/policies.component.ts
@@ -1,6 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { LogService } from '@cisco-ngx/cui-services';
+import {
+	ControlPointDevicePolicyAPIService,
+	PolicyResponseModel,
+	UserService,
+} from '@sdp-api';
+
+import { empty, Subject } from 'rxjs';
+import { catchError, finalize, takeUntil, mergeMap } from 'rxjs/operators';
+
+import * as _ from 'lodash-es';
 
 /**
  * Main Policies component
@@ -10,8 +20,12 @@ import { LogService } from '@cisco-ngx/cui-services';
 	styleUrls: ['./policies.component.scss'],
 	templateUrl: './policies.component.html',
 })
-export class PoliciesComponent {
-
+export class PoliciesComponent implements OnInit {
+	private destroyed$: Subject<void> = new Subject<void>();
+	public policyData: PolicyResponseModel[];
+	public loading = true;
+	public error = false;
+	public errorMessage: string;
 	public scans = [
 		{
 			enabled: true,
@@ -39,7 +53,78 @@ export class PoliciesComponent {
 
 	constructor (
 		private logger: LogService,
+		private controlPointDevicePolicyAPIService: ControlPointDevicePolicyAPIService,
+		private userService: UserService,
 	) {
 		this.logger.debug('PoliciesComponent Created!');
+	}
+
+	/**
+	 * Get policy info given customerID
+	 * @param {String} customerId - Customer ID string
+	 * @returns observable
+	 */
+	public getPoliciesData (customerId: string) {
+		return this.controlPointDevicePolicyAPIService.getAllPolicyUsingGET(customerId)
+			.pipe(
+				catchError(err => {
+					this.error = true;
+					this.errorMessage = err.message;
+
+					return empty();
+				}),
+				finalize(() => this.loading = false),
+				takeUntil(this.destroyed$),
+			);
+	}
+
+	/**
+	 * Handles seperates policyData into scan and colleciton lists
+	 */
+	public handleData () {
+		this.scans = [];
+		this.collections = [];
+		_.forEach(this.policyData, element => {
+			const ptype = _.get(element, 'policyType');
+			if (ptype === 'SCAN') {
+				this.scans.push(element);
+			} else if (ptype === 'COLLECTION') {
+				this.collections.push(element);
+			}
+		});
+	}
+
+	/**
+	 * NgOnDestroy
+	 */
+	public ngOnDestroy () {
+		this.destroyed$.next();
+		this.destroyed$.complete();
+	}
+
+	/**
+	 * Function which instanstiates the settings page to the initial view
+	 */
+	public ngOnInit () {
+		this.loading = true;
+		this.userService.getUser()
+			.pipe(
+				catchError(err => {
+					this.error = true;
+					this.errorMessage = err.message;
+
+					return empty();
+				}),
+				finalize(() => this.loading = false),
+				takeUntil(this.destroyed$),
+			)
+			.pipe(
+				mergeMap(userResponse =>
+					this.getPoliciesData(String(_.get(userResponse, 'data.customerId')))),
+			)
+			.subscribe(response => {
+				this.policyData = response;
+				this.handleData();
+			});
 	}
 }
