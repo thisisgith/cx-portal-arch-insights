f7ea0c41729cddb77c4e5a7f2ae42a433a40150e
Gordon Fedoriw <fedoriw@cisco.com>
2019-07-30 14:38:11 -0400

    initial push for PBC-344

--- cypress/integration/assets_spec.js
+++ cypress/integration/assets_spec.js
@@ -696,4 +696,24 @@ describe('Assets', () => { // PBC-41
 			cy.get('table').should('be.visible');
 		});
 	});
+
+	context.only('PBC-344: Asset Cases - Asset Based Case Open', () => {
+
+		it('Provides an Asset 360 view modal', () => { 
+			const validate360OpenCase = asset => {
+				const haveVisibility = asset.supportCovered ? 'be.visible' : 'not.be.visible';
+				cy.getByAutoId('Asset360ScanBtn').should('be.visible');
+				cy.getByAutoId('Asset360OpenCaseBtn').should(haveVisibility).click(); // PBC344
+				cy.getByAutoId('CaseOpenCancelButton').should('exist').click(); // cancel it
+				cy.getByAutoId('CaseOpenCancel').should('exist').click(); // confirm cancel
+			};
+
+			cy.get('[data-auto-id="AssetsTableBody"] tr').eq(0).click();
+			validate360OpenCase(assets[0]);
+
+			cy.getByAutoId('CloseDetails').click();
+		});
+
+
+	});
 });
