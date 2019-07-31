72f4e4c1910a0106ffe007d12269a58bdfd89805
Zachery Thomas (zacthoma) <zacthoma@cisco.com>
2019-07-31 13:36:53 -0400

    Fix linting errors

--- src/app/pages/admin/policies/policy-form/policy-form.component.spec.ts
+++ src/app/pages/admin/policies/policy-form/policy-form.component.spec.ts
@@ -1,67 +1,43 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { PolicyFormComponent } from './policy-form.component';
 import { PolicyFormModule } from './policy-form.module';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { By } from '@angular/platform-browser';
+
+import { Location } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import {  RacetrackContentService } from '@sdp-api';
-import {
-	ACCUserInfoScenarios,
-	Mock,
-} from '@mock';
-import * as _ from 'lodash-es';
-import { of } from 'rxjs';
+import { PolicyResponseModel } from '@sdp-api';
+import { PolicesScenarios } from '@mock';
+import { RouterTestingModule } from '@angular/router/testing';
 
-/**
- * Will fetch the currently active response body from the mock object
- * @param mock the mock object
- * @param type the scenario type
- * @returns the body response
- */
-function getActiveBody (mock: Mock, type: string = 'GET') {
-	const active = _.find(mock.scenarios[type], 'selected') || _.head(mock.scenarios[type]);
-
-	return active.response.body;
-}
 
 describe('PolicyFormComponent', () => {
 	let component: PolicyFormComponent;
 	let fixture: ComponentFixture<PolicyFormComponent>;
-	let contentService: RacetrackContentService;
 
-	let accUserInfoSpy;
-
-	/**
-	 * Restore spies
-	 */
-	const restoreSpies = () => {
-		_.invoke(accUserInfoSpy, 'restore');
-	};
-
-	const buildSpies = () => {
-		accUserInfoSpy = spyOn(contentService, 'getACCUserInfo')
-			.and
-			.returnValue(of(getActiveBody(ACCUserInfoScenarios[0])));
+	const locationStub = {
+		back: jasmine.createSpy('back'),
 	};
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			imports: [
-				PolicyFormModule,
-				FormsModule,
 				HttpClientTestingModule,
-				ReactiveFormsModule,
+				PolicyFormModule,
+				RouterTestingModule,
+			],
+			providers: [
+				{
+					provide: Location,
+					useValue: locationStub,
+				},
 			],
 		})
 		.compileComponents();
-
-		contentService = TestBed.get(RacetrackContentService);
 	}));
 
 	beforeEach(() => {
 		fixture = TestBed.createComponent(PolicyFormComponent);
 		component = fixture.componentInstance;
-		restoreSpies();
 		fixture.detectChanges();
 	});
 
@@ -69,39 +45,4 @@ describe('PolicyFormComponent', () => {
 		expect(component)
 			.toBeTruthy();
 	});
-
-	it('should have loaded customer info', () => {
-		buildSpies();
-		component.ngOnInit();
-		fixture.detectChanges();
-		expect(component.custData)
-			.toBeTruthy();
-	});
-
-	it('should have invalid form when empty', () => {
-		expect(component.requestForm.valid)
-			.toBeFalsy();
-	});
-
-	it('should add additional attendees', () => {
-		const select = fixture.debugElement.query(By.css('#attendees-select'));
-
-		const attendees = component.requestForm.controls.attendees;
-		expect(attendees.valid)
-			.toBeFalsy();
-
-		attendees.setValue('2');
-		fixture.detectChanges();
-		expect(select.nativeElement.value)
-			.toEqual('2');
-		expect(attendees.hasError('required'))
-			.toBeFalsy();
-
-		component.addAdditionalAttendeeForms(select.nativeElement.value);
-
-		const additionalAttendees = component.requestForm.get('additionalAttendees');
-		expect(additionalAttendees.value.length)
-			.toEqual(1);
-	});
-
 });
