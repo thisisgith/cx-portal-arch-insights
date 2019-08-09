7ac82275bbe07cd51157ce81e4443b26f2841448
jakparks <jakparks@cisco.com>
2019-08-09 07:56:32 -0700

    Case open refactors

--- src/app/components/case/case-open/case-open.component.spec.ts
+++ src/app/components/case/case-open/case-open.component.spec.ts
@@ -1,11 +1,11 @@
 import { async, fakeAsync, tick, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormGroup } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
 
 import { CaseScenarios } from '@mock';
 import { ProfileService } from '@cisco-ngx/cui-auth';
-import { CaseRequestType } from '@classes';
 import { CuiModalService } from '@cisco-ngx/cui-components';
 import { CloseConfirmComponent } from './close-confirm/close-confirm.component';
 import { CaseOpenComponent } from './case-open.component';
@@ -62,18 +62,6 @@ describe('CaseOpenComponent', () => {
 			.toHaveBeenCalledWith(CloseConfirmComponent, { });
 	}));
 
-	it('should refresh problemArea options on RMA change', fakeAsync(() => {
-		spyOn(caseService, 'fetchProblemArea')
-			.and
-			.returnValue(
-				of(null),
-			);
-		component.caseForm.controls.requestRma.setValue(true);
-		tick();
-		expect(caseService.fetchProblemArea)
-			.toHaveBeenCalledWith(CaseRequestType.RMA);
-	}));
-
 	it('should expand', fakeAsync(() => {
 		component.caseForm.controls.description.setValue('a');
 		component.caseForm.controls.title.setValue('a');
@@ -98,14 +86,15 @@ describe('CaseOpenComponent', () => {
 		button.nativeElement.click();
 		tick();
 		fixture.detectChanges(); // click next and expand
-		component.caseForm.controls.problemArea.setValue({
+		(<FormGroup> component.caseForm.controls.techInfo).controls.problemArea.setValue({
 			customerActivity: 'Operate',
 			problemCode: 'PASSWORD_RECOV',
 			problemCodeName: 'Password Recovery',
 		});
 		component.caseForm.controls.severity.setValue(3);
 		component.caseForm.controls.requestRma.setValue(false);
-		component.caseForm.controls.technology.setValue('10');
+		(<FormGroup> component.caseForm.controls.techInfo).controls.technology.setValue('10');
+		(<FormGroup> component.caseForm.controls.techInfo).controls.subtech.setValue('33');
 		fixture.detectChanges(); // enable submit button
 		const submitButton = fixture.debugElement.query(
 			By.css('[data-auto-id="CaseOpenSubmitButton"]'),
