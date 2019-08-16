1b76b70513ed978aa04debd6671fcd7f99f64ce8
swmamidi <swmamidi@cisco.com>
2019-08-16 11:06:41 -0400

    fixing lint errors

--- src/app/pages/solution/lifecycle/lifecycle.component.spec.ts
+++ src/app/pages/solution/lifecycle/lifecycle.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, tick, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { LifecycleComponent } from './lifecycle.component';
 import { LifecycleModule } from './lifecycle.module';
@@ -638,7 +638,7 @@ describe('LifecycleComponent', () => {
 			sendParams();
 			spyOn(component, 'updateBookmark');
 			fixture.detectChanges();
-			
+
 			de = fixture.debugElement.query(By.css('#hover-panel-successbytes .icon-bookmark'));
 			de.nativeElement.click();
 			expect(component.updateBookmark)
