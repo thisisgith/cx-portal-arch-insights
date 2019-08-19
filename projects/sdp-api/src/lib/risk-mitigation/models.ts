728073927b68304aff467503a762004cb9b9f5af
msangwan <msangwan@cisco.com>
2019-08-19 14:51:49 +0530

    fixing lint  error in sdp

--- projects/sdp-api/src/lib/risk-mitigation/models.ts
+++ projects/sdp-api/src/lib/risk-mitigation/models.ts
@@ -1,6 +1,11 @@
 export { RiskAssets } from './models/risk-assets';
 export { RiskAsset } from './models/risk-asset';
 export { RiskPagination as RiskMitigationPagination } from './models/risk-pagination';
-export { HighCrashRisk, HighCrashRiskDevices, HighCrashRiskPagination, HighCrashRiskDeviceCount } from './models/high-crash-risk';
+export {
+	HighCrashRisk,
+	HighCrashRiskDevices,
+	HighCrashRiskPagination,
+	HighCrashRiskDeviceCount,
+} from './models/high-crash-risk';
 export { Filter as RmFilter } from './models/filter';
 export { CrashCount } from './models/crash-count';
