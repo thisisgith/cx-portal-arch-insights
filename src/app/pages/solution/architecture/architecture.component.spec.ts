9ae6f61fe0c0b0838c9eff7ef88daba887f06be9
Prashanth <prasred2@cisco.com>
2019-08-21 08:40:59 +0530

    m

--- src/app/pages/solution/architecture/architecture.component.spec.ts
+++ src/app/pages/solution/architecture/architecture.component.spec.ts
@@ -5,6 +5,8 @@ import { ArchitectureModule } from './architecture.module';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ArchitectureService } from '@sdp-api';
 import { of } from 'rxjs';
+import * as _ from 'lodash-es';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ArchitectureComponent', () => {
 	let component: ArchitectureComponent;
@@ -15,13 +17,14 @@ describe('ArchitectureComponent', () => {
 		TestBed.configureTestingModule({
 			imports: [ArchitectureModule,
 				HttpClientTestingModule,
+				RouterTestingModule,
 			],
 		})
 		.compileComponents();
+		service = TestBed.get(ArchitectureService);
 	}));
 
 	beforeEach(() => {
-		service = TestBed.get(ArchitectureService);
 		spyOn(service, 'getExceptionsCount')
 			.and
 			.returnValue(of({ CBPRulesCount: 5000 }));
@@ -48,8 +51,61 @@ describe('ArchitectureComponent', () => {
 			.toHaveBeenCalled();
 	});
 
-	it('should select the tab on click', () => {
-		expect(component.selectVisualLabel('Configuration Best Practices || Assets With Exception'))
-			.toHaveBeenCalled();
+	it('should clear the filters on clear button', done => {
+		fixture.whenStable()
+			.then(() => {
+				fixture.detectChanges();
+				const exceptionFilter = _.find(this.filters, { key: 'exceptions' });
+				component.onSubfilterSelect('none', exceptionFilter);
+
+				fixture.detectChanges();
+
+				expect(_.filter(component.filters, 'selected'))
+					.toContain(exceptionFilter);
+
+				const subfilter = _.find(exceptionFilter.seriesData,
+					 { filter: 'High' || 'Medium' || 'Low' });
+
+				expect(subfilter.selected)
+					.toBeTruthy();
+
+				component.clearFilters();
+				fixture.detectChanges();
+
+				expect(subfilter.selected)
+					.toBeFalsy();
+
+				done();
+			});
+	});
+
+	it('should clear the filter when selecting the same subfilter twice', done => {
+		fixture.whenStable()
+			.then(() => {
+				fixture.detectChanges();
+				const exceptionFilter = _.find(this.filters, { key: 'exceptions' });
+				component.onSubfilterSelect('none', exceptionFilter);
+
+				fixture.detectChanges();
+
+				expect(_.filter(component.filters, 'selected'))
+					.toContain(exceptionFilter);
+
+				let subfilter = _.find(exceptionFilter.seriesData, { filter: 'none' });
+
+				expect(subfilter.selected)
+					.toBeTruthy();
+
+				component.onSubfilterSelect('none', exceptionFilter);
+
+				fixture.detectChanges();
+
+				subfilter = _.find(exceptionFilter.seriesData, { filter: 'none' });
+
+				expect(subfilter.selected)
+					.toBeFalsy();
+
+				done();
+			});
 	});
 });
