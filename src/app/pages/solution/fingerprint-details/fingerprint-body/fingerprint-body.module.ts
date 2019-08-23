f827ac2bb665f4cfbd3341967336d36abcfb7f0d
Shrinidhi Patil (shrinipa) <shrinipa@cisco.com>
2019-08-23 21:22:32 +0530

    fixed typo in import

--- src/app/pages/solution/fingerprint-details/fingerprint-body/fingerprint-body.module.ts
+++ src/app/pages/solution/fingerprint-details/fingerprint-body/fingerprint-body.module.ts
@@ -5,7 +5,7 @@ import { FpCompareModule } from './fp-compare/fp-compare.module';
 import { I18nPipeModule } from '@cisco-ngx/cui-pipes';
 import { CuiTabsModule } from '@cisco-ngx/cui-components';
 import { FpCompareComponent } from './fp-compare/fp-compare.component';
-import { FpIntelligenceModule } wfrom '../fp-intelligence/fp-intelligence.module';
+import { FpIntelligenceModule } from '../fp-intelligence/fp-intelligence.module';
 import { FpSimilarAssetsModule } from '../fp-similarassets/fp-similarassets.module';
 
 /**
