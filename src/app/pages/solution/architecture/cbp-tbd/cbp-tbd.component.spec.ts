cb169d9294ac30c95e9a9999ecd49af2dec8cb6a
Prashanth <prasred2@cisco.com>
2019-08-28 17:08:36 +0530

    code review changes

--- src/app/pages/solution/architecture/cbp-tbd/cbp-tbd.component.spec.ts
+++ src/app/pages/solution/architecture/cbp-tbd/cbp-tbd.component.spec.ts
@@ -1,40 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { user } from '@mock';
 import { CbpTbdComponent } from './cbp-tbd.component';
-import { CbpTbdModule } from './cbp-tbd.module';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ArchitectureService, IAsset } from '@sdp-api';
-import { of, throwError } from 'rxjs';
-import { RouterTestingModule } from '@angular/router/testing';
-import { environment } from '@environment';
-import { ActivatedRoute } from '@angular/router';
-import { HttpErrorResponse } from '@angular/common/http';
+import { ArchitectureService } from '@sdp-api';
+import { of } from 'rxjs';
+import { CbpTbdModule } from './cbp-tbd.module';
 
 describe('CbpTbdComponent', () => {
 	let component: CbpTbdComponent;
 	let fixture: ComponentFixture<CbpTbdComponent>;
 	let service: ArchitectureService;
-	const cbpDetails: IAsset = new Object();
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			imports: [CbpTbdModule,
 				HttpClientTestingModule,
-				RouterTestingModule,
-			],
-			providers: [
-				{ provide: 'ENVIRONMENT', useValue: environment },
-				{
-					provide: ActivatedRoute,
-					useValue: {
-						queryParams: of({ }),
-						snapshot: {
-							data: {
-								user,
-							},
-						},
-					},
-				},
 			],
 		})
 		.compileComponents();
@@ -55,33 +34,10 @@ describe('CbpTbdComponent', () => {
 			.toBeTruthy();
 	});
 
-	it('should set null values on request errors', () => {
-		const error = {
-			status: 404,
-			statusText: 'Resource not found',
-		};
-		spyOn(service, 'getCBPSeverityList')
-			.and
-			.returnValue(
-				throwError(new HttpErrorResponse(error)),
-			);
-		component.getData();
-	});
-
 	it('should call getAllCBPExceptionDetails', () => {
 		component.getData();
 		expect(service.getAllCBPExceptionDetails)
 			.toHaveBeenCalled();
-	});
-
-	it('should change params on onchange', () => {
-		component.ngOnChanges();
-		if (cbpDetails) {
-			expect(component.isLoading)
-			.toBeTruthy();
-			expect(component.params.page)
-			.toBe(0);
-		}
 	});
 
 	it('should update pagination params', () => {
