4989f0ca3996b996d657860a2a34363b22e0d8b9
teraja <teraja@cisco.com>
2019-08-29 10:49:20 +0530

    removed unwanted files

--- src/app/pages/solution/architecture-review/dnac-list/dnac-list.module.ts
+++ src/app/pages/solution/architecture-review/dnac-list/dnac-list.module.ts
@@ -3,10 +3,8 @@ import { CommonModule } from '@angular/common';
 import { DnacListComponent } from './dnac-list.component';
 import { CuiTableModule, CuiPagerModule, CuiSpinnerModule,
 	CuiDropdownModule } from '@cisco-ngx/cui-components';
-import { DetailsPanelModule } from '@components';
 import { I18nPipeModule } from '@cisco-ngx/cui-pipes';
 import { FormsModule } from '@angular/forms';
-import { DnacDetailsModule } from '../dnac-details/dnac-details.module';
 
 /** Module representing the Devices With Exceptions Component */
 @NgModule({
@@ -17,8 +15,6 @@ import { DnacDetailsModule } from '../dnac-details/dnac-details.module';
 		CuiDropdownModule,
 		CuiTableModule,
 		CuiPagerModule,
-		DetailsPanelModule,
-		DnacDetailsModule,
 		I18nPipeModule,
 		CuiSpinnerModule,
 		FormsModule,
