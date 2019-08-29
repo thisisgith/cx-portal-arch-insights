fd229a30bcb4a4828576a6b8729cbd1842154cbd
teraja <teraja@cisco.com>
2019-08-29 18:18:35 +0530

    fixed lint and review issues

--- src/app/pages/solution/architecture-review/dnac-list/dnac-list.component.ts
+++ src/app/pages/solution/architecture-review/dnac-list/dnac-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { I18n } from '@cisco-ngx/cui-utils';
 import { LogService } from '@cisco-ngx/cui-services';
 import { CuiTableOptions } from '@cisco-ngx/cui-components';
-import { ArchitectureReviewService, assetExceptionList } from '@sdp-api';
+import { ArchitectureReviewService, assetExceptionList, IParamType } from '@sdp-api';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import { DatePipe } from '@angular/common';
@@ -44,7 +44,7 @@ export class DnacListComponent implements OnInit {
 	private destroy$ = new Subject();
 	public globalSearchText  = '';
 	public lastCollectionTime = '';
-	public params = { customerId : '' , page: 0, pageSize: 10, searchText : '' };
+	public params: IParamType = { customerId : '' , page: 0, pageSize: 10, searchText : '' };
 	public fullscreen: any ;
 	@ViewChild('devicesTemplate', { static: true })
 	private devicesTemplate: TemplateRef<{ }>;
