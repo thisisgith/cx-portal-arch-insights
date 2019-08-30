4f84c943c5e6c85896db425da8815f3589925ca0
Lakshmikant Deshpande <ladeshpa@cisco.com>
2019-08-31 02:12:36 +0530

    changes

--- src/app/pages/solution/insights/insights.component.spec.ts
+++ src/app/pages/solution/insights/insights.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { InsightsComponent } from './insights.component';
 import { InsightsModule } from './insights.module';
@@ -8,10 +8,14 @@ import { OptimalSoftwareVersionComponent } from '../osv/osv.component';
 import { RiskMitigationComponent } from '../risk-mitigation/risk-mitigation.component';
 import { OptimalSoftwareVersionModule } from '../osv/osv.module';
 import { RiskMitigationModule } from '../risk-mitigation/risk-mitigation.module';
+import { RouteAuthService } from 'src/app/services';
+import { of, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 describe('InsightsComponent', () => {
 	let component: InsightsComponent;
 	let fixture: ComponentFixture<InsightsComponent>;
+	let routeAuthService: RouteAuthService;
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
@@ -33,6 +37,7 @@ describe('InsightsComponent', () => {
 			],
 		})
 			.compileComponents();
+		routeAuthService = TestBed.get(RouteAuthService);
 	}));
 
 	beforeEach(() => {
@@ -47,11 +52,43 @@ describe('InsightsComponent', () => {
 			.toBeTruthy();
 	});
 
-	it('should get the optin status for loggedIn user', () => {
+	/**
+	 * @TODO: modify test to use UI
+	 */
+	it('should set empty select', fakeAsync(() => {
+		const error = {
+			status: 404,
+			statusText: 'Resource not found',
+		};
+		spyOn(routeAuthService, 'checkPermissions')
+			.and
+			.returnValue(throwError(new HttpErrorResponse(error)));
+		tick();
+		fixture.detectChanges();
+		expect(component.hasPermission)
+			.toEqual(false);
+	}));
+
+	it('Should return the user permission response true', fakeAsync(() => {
 		const response = true;
+		spyOn(routeAuthService, 'checkPermissions')
+			.and
+			.returnValue(of(response));
+		tick(1000);
+		fixture.detectChanges();
 		expect(component.hasPermission)
-				.toBeDefined();
+			.toBeDefined();
+	}));
+
+	it('Should return the user permission response false', fakeAsync(() => {
+		const response = false;
+		spyOn(routeAuthService, 'checkPermissions')
+			.and
+			.returnValue(of(response));
+		tick(1000);
+		fixture.detectChanges();
 		expect(component.hasPermission)
-				.toEqual(response);
-	});
+			.toBeDefined();
+	}));
+
 });
