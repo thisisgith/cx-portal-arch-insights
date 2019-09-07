266818f6343ee74a73e8cee7e0dc4d8d5f045df5
Lakshmikant Deshpande <ladeshpa@cisco.com>
2019-09-07 16:26:52 +0530

    RCC : spec file code clean up

--- src/app/pages/solution/rcc/rcc-asset-violation-details/rcc-asset-violation-details.component.spec.ts
+++ src/app/pages/solution/rcc/rcc-asset-violation-details/rcc-asset-violation-details.component.spec.ts
@@ -211,17 +211,6 @@ describe('RccAssetViolationDetailsComponent', () => {
 	});
 
 	it('should invoke onPageIndexChange method', () => {
-		component.assetRowParams = {
-			customerId: '7293498',
-			pageIndex: 0,
-			pageSize: 10,
-			policyGroupName: '',
-			policyName: '',
-			serialNumber: 'FCW2246E0PB',
-			severity: '',
-			sortBy: '',
-			sortOrder: '',
-		};
 		component.onPolicyAssetPagerUpdated({ page: 1, limit : 10 });
 		expect(component.tableOffset)
 			.toEqual(1);
