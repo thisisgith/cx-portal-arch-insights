6dc6864ee92f82ceb232b402ecb33cb7bf96d79c
Carl Lindner <clindner@cisco.com>
2019-09-11 16:11:00 -0400

    Another 2FA change

--- deploy-s3-lt.sh
+++ deploy-s3-lt.sh
@@ -8,15 +8,6 @@ function resetAws () {
 	export AWS_SESSION_TOKEN=`echo $out | grep -Eow 'SessionToken": ".*?"' | cut -f 2 -d " " | tr -d \"`
 }
 
-function resetAwsNo2FA () {
-	unset AWS_ACCESS_KEY_ID ; unset AWS_SECRET_ACCESS_KEY ; unset AWS_SESSION_TOKEN
-	out=`aws sts get-session-token --serial-number arn:aws:iam::728174137439:mfa/$USER`
-	echo $out
-	export AWS_ACCESS_KEY_ID=`echo $out | grep -Eow 'AccessKeyId": ".*?"' | cut -f 2 -d " " | tr -d \"`
-	export AWS_SECRET_ACCESS_KEY=`echo $out | grep -Eow 'SecretAccessKey": ".*?"' | cut -f 2 -d " " | tr -d \"`
-	export AWS_SESSION_TOKEN=`echo $out | grep -Eow 'SessionToken": ".*?"' | cut -f 2 -d " " | tr -d \"`
-}
-
 # Note, as an API user Jenkins deployments do no require 2FA, otherwise you must include the authy-token
 echo "Usage as user: ./deploy-s3.sh <authy-token>"
 echo "Usage as Jenkins:  ./deploy-s3-lt.sh"
@@ -24,7 +15,6 @@ echo "Usage as Jenkins:  ./deploy-s3-lt.sh"
 # set AWS session env vars
 if [ "$#" -ne 1 ]; then
   echo "Assuming you are the Jenkins user"
-	resetAwsNo2FA
 else
   echo "Assuming you are an API user and have provided an authy-token"
   resetAws $1
