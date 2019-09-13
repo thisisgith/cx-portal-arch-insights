17919f57cd9db444c0ec86be86d0eea2026e118a
Gordon Fedoriw <fedoriw@cisco.com>
2019-09-13 11:09:17 -0400

    push for PBC-635

--- cypress/integration/insights_spec.js
+++ cypress/integration/insights_spec.js
@@ -2,21 +2,7 @@ import MockService from '../support/mockService';
 
 const i18n = require('../../src/assets/i18n/en-US.json');
 
-// const searchMock = new MockService('SearchScenarios');
-
 describe('Insights Spec', () => {
-	context('Basic Loading Sanity', () => {
-		// it('Loads the app', () => {
-		// 	cy.loadApp();
-		// 	// cy.get('h1.page-title').should('have.text', 'CX Console');
-		// });
-
-		it('Requires SSO login', () => {
-			MockService.mockUnauthenticatedUser();
-			cy.loadApp();
-			cy.url().should('contain', 'https://cloudsso-test.cisco.com/idp/SSO.saml2');
-		});
-	});
 
 	context(' Verify Insights Tab ', () => {
 		before(() => {
@@ -30,23 +16,24 @@ describe('Insights Spec', () => {
 			cy.getByAutoId('ConfigurationTabNav').should('exist');
 			cy.getByAutoId('SyslogsTabNav').should('exist').should('contain', i18n._Syslogs_);
 			cy.getByAutoId('Architecture ReviewTabNav').should('exist');
-			cy.getByAutoId('SoftwareTabNav').click();
+			cy.getByAutoId('SoftwareTabNav').click({ force: true });
 			cy.getByAutoId('ContactExpert').should('exist').should('contain', i18n._CSRequestExpert_);
-			cy.getByAutoId('Risk MitigationTabNav').click();
+			cy.getByAutoId('Risk MitigationTabNav').click({ force: true });
 			// TODO check for _CP_FingerprintIntelligence_ tab should exist if/when there is data
-			// TODO check for the Compare tab
+			// TODO check for the Compare tab?
 		});
 		it.skip('PBC-635 Verify Insights Tab for cxlevel 2 or 1', () => {
+			// Skip for now: a real user with  cxlevel 1 or 2 does not exist at this time.
+			// Verified:  Manually changed cxLevel: 2 or 1 in entitlement.ts - Test passes
 			// TODO Switch to/Find a userID with cxlevel 2 - items below s/b hidden
-			// Skip for now
 			cy.getByAutoId('ConfigurationTabNav').should('not.be.visible');
 			cy.getByAutoId('SyslogsTabNav').should('not.be.visible');
 			cy.getByAutoId('Architecture ReviewTabNav').should('not.be.visible');
-			cy.getByAutoId('SoftwareTabNav').click();
-			cy.getByAutoId('ContactExpert').should('exist').should('not.be.visible');
-			cy.getByAutoId('Risk MitigationTabNav').click();
+			cy.getByAutoId('SoftwareTabNav').click({ force: true });
+			cy.getByAutoId('ContactExpert').should('not.be.visible');
+			cy.getByAutoId('Risk MitigationTabNav').click({ force: true });
 			// TODO check _CP_FingerprintIntelligence_ tab should not.be.visible if/when there is data
-			// TODO check the Compare tab should not.be.visible
+			// TODO check the Compare tab should not.be.visible?
 		});
 
 	});
