be6a7ed545c89ca2668618814c52f942ce170f7a
apulkuma <apulkuma@cisco.com>
2019-10-09 18:34:03 +0530

    CSCvr48967_localStorageRemoval

--- src/app/pages/solution/resolution/resolution.component.ts
+++ src/app/pages/solution/resolution/resolution.component.ts
@@ -349,50 +349,47 @@ export class ResolutionComponent implements OnInit, OnDestroy {
 	 * Initializes the filters at half-opacity with stored values.
 	 */
 	private initializeFilters () {
-		const caseFilterData = JSON.parse(
-			localStorage.getItem('caseFilterData'),
-		) || defaultFiltersData;
 		this.filters = [
 			{
 				key: 'total',
 				loading: true,
 				selected: true,
-				seriesData: caseFilterData.total,
+				seriesData: defaultFiltersData.total,
 				template: this.totalFilterTemplate,
 				title: I18n.get('_Total_'),
 			},
 			{
 				key: 'status',
 				loading: true,
-				seriesData: caseFilterData.status,
+				seriesData: defaultFiltersData.status,
 				template: this.pieChartFilterTemplate,
 				title: I18n.get('_Status_'),
 			},
 			{
 				key: 'severity',
 				loading: true,
-				seriesData: caseFilterData.severity,
+				seriesData: defaultFiltersData.severity,
 				template: this.pieChartFilterTemplate,
 				title: I18n.get('_Severity_'),
 			},
 			{
 				key: 'lastUpdated',
 				loading: true,
-				seriesData: caseFilterData.lastUpdated,
+				seriesData: defaultFiltersData.lastUpdated,
 				template: this.columnChartFilterTemplate,
 				title: I18n.get('_RMACaseUpdatedDate_'),
 			},
 			{
 				key: 'durationOpen',
 				loading: true,
-				seriesData: caseFilterData.durationOpen,
+				seriesData: defaultFiltersData.durationOpen,
 				template: this.columnChartFilterTemplate,
 				title: I18n.get('_TotalTimeOpen_'),
 			},
 			{
 				key: 'rma',
 				loading: true,
-				seriesData: caseFilterData.rma,
+				seriesData: defaultFiltersData.rma,
 				template: this.barChartFilterTemplate,
 				title: I18n.get('_RMAs_'),
 			},
@@ -456,7 +453,6 @@ export class ResolutionComponent implements OnInit, OnDestroy {
 			takeUntil(this.destroy$),
 		)
 		.subscribe(cases => {
-			this.isLoading = false;
 			this.caseListData = cases.content;
 
 			const first = (this.caseParams.size * (this.paginationInfo.currentPage)) + 1;
@@ -480,6 +476,8 @@ export class ResolutionComponent implements OnInit, OnDestroy {
 			if (!this.builtFilters) {
 				this.buildFilters();
 				this.builtFilters = true;
+			} else {
+				this.isLoading = false;
 			}
 		}, err => {
 			this.isLoading = false;
@@ -505,6 +503,7 @@ export class ResolutionComponent implements OnInit, OnDestroy {
 			this.getFilterData(response.totalElements);
 			this.logger.debug('resolution.component : buildFilters() :: Finished building filters');
 		}, err => {
+			this.isLoading = false;
 			if (window.Cypress) {
 				window.loading = false;
 			}
@@ -615,8 +614,6 @@ export class ResolutionComponent implements OnInit, OnDestroy {
 			severityFilter.seriesData = _.sortBy(severityFilter.seriesData,
 				seriesData => seriesData.filter);
 
-			// Finish loading and save data in localStorage
-			const caseFilterData = { };
 			_.each(this.filters, filter => {
 				if (_.includes(['status', 'severity'], filter.key)) {
 					// Don't include status or severities in pie charts with no associated cases
@@ -624,16 +621,15 @@ export class ResolutionComponent implements OnInit, OnDestroy {
 				}
 				_.set(filter, 'seriesData', [...filter.seriesData]);
 				_.set(filter, 'loading', false);
-				caseFilterData[filter.key] = filter.seriesData.map(data =>
-					_.pick(data, ['barLabel', 'filter', 'label', 'value']));
 			});
-			localStorage.setItem('caseFilterData', JSON.stringify(caseFilterData));
+			this.isLoading = false;
 
 			if (window.Cypress) {
 				window.loading = false;
 			}
 			this.logger.debug('resolution.component : getFilterData() :: Finished getting data');
 		}, err => {
+			this.isLoading = false;
 			if (window.Cypress) {
 				window.loading = false;
 			}
