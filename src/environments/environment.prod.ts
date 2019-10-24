5759cc4718864a2403b81745c8943fcee92c179f
Mike Hart <michhart@cisco.com>
2019-10-25 04:55:51 +0530

    fix: Update mapbox token for production environment
    Resolves [CSCvr79033](https://cdetsng.cisco.com/webui/#view=CSCvr79033)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -56,7 +56,7 @@ export const environment = {
 	learningLink: 'https://digital-learning.cisco.com/cx#/',
 	logoutUrl: 'https://www.cisco.com/autho/logout.html',
 	mapboxToken:
-	'pk.eyJ1IjoibWlzY2htaTIiLCJhIjoiY2p4eDl2ODVnMGpmODNkcDhsbG5nNXQ0YyJ9.vpfGffFASSUfANAU4Yvtxg',
+	'pk.eyJ1IjoibWljaGhhcnQiLCJhIjoiY2syNWMwMWt6MDAzYTNyczkxdjI2dWxsdSJ9.O3GKDafZWz1gpA4dJ6msHw',
 	mock: [],
 	myCase: 'https://mycase.cloudapps.cisco.com',
 	noAuthEndpoints: [
