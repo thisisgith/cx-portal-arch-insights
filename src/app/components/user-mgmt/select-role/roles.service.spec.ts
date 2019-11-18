721c56a8ec41510df4039727dd912bd4e00c688b
taylordeckard <taylor999@gmail.com>
2019-11-18 14:48:11 -0500

    [fixup]

--- src/app/components/user-mgmt/select-role/roles.service.spec.ts
+++ src/app/components/user-mgmt/select-role/roles.service.spec.ts
@@ -5,7 +5,7 @@ import { ControlPointUserManagementAPIService } from '@sdp-api';
 import { RouterTestingModule } from '@angular/router/testing';
 import { inject, TestBed } from '@angular/core/testing';
 import { environment } from '@environment';
-import { of } from 'rxjs';
+import { of, asyncScheduler } from 'rxjs';
 
 describe('RolesService', () => {
 	let api: ControlPointUserManagementAPIService;
@@ -30,7 +30,7 @@ describe('RolesService', () => {
 			.returnValue(of({
 				saRoles: [],
 				vaRoles: [],
-			}));
+			}, asyncScheduler));
 	});
 
 	it('should be created', inject([RolesService], (service: RolesService) => {
