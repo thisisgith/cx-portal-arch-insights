e0187502e4e916b3e1efba97bbb04ffa138d7139
swatsin2 <swatsin2@cisco.com>
2019-11-19 20:50:36 +0530

    redownload collector ui

--- src/app/pages/setup-ie/register-collector/register-collector.component.ts
+++ src/app/pages/setup-ie/register-collector/register-collector.component.ts
@@ -388,7 +388,16 @@ export class RegisterCollectorComponent implements OnDestroy, OnInit, SetupStep 
 							.pipe(takeUntil(this.destroyed$))
 							.subscribe(() => {
 								// continue to next screen after a second
-								this.onStepComplete.emit();
+								this.route.queryParams.subscribe(params => {
+									if (params.fromAdmin) {
+										// tslint:disable-next-line: no-console
+										console.log('from admin');
+										this.onStepComplete.emit();
+										this.router.navigate(['/setup-ie']);
+									} else {
+										this.onStepComplete.emit();
+									}
+								});
 							});
 
 						return false;
