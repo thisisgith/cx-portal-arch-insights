c78fbcddd91cad3d5627c79df28526af26b7a1d9
ambikpat <ambikpat@cisco.com>
2019-11-19 14:36:37 +0530

    fixed review comments

--- src/app/pages/solution/fingerprint-details/fingerprint-body/fp-compare/comparisonview/comparisonview.component.ts
+++ src/app/pages/solution/fingerprint-details/fingerprint-body/fp-compare/comparisonview/comparisonview.component.ts
@@ -136,6 +136,7 @@ export class ComparisonviewComponent {
 		};
 
 		return this.assetPanelLinkService.getAssetLinkData(this.assetParams)
+		.pipe(takeUntil(this.destroy$))
 		.subscribe(response => {
 			this.assetLinkInfo.asset = _.get(response, [0, 'data', 0]);
 			this.assetLinkInfo.element = _.get(response, [1, 'data', 0]);
