a6216d1548ec8ed305bf20fa363b2e20d110fbcb
skudlisr <skudlisr@cisco.com>
2019-11-20 17:14:54 -0500

    feat(framework): Allows GUI to call region specific APIs by reading preferred datacenter value
    Adds a new interceptor to route APIs to user preferred datacenter
    
    Resolves US147392

---
+++ src/app/interceptors/apix-datacenter.interceptor.spec.ts
@@ -0,0 +1,89 @@
+
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { HttpRequest, HttpHeaders, HttpParams, HttpHandler, HttpEvent } from '@angular/common/http';
+import { environment } from '@environment';
+import { from, Observable } from 'rxjs';
+import { ApixDatacenterInterceptor } from './apix-datacenter.interceptor';
+import { UserResolve } from '@utilities';
+import { ApixIdentityService } from './apix-identity.service';
+import { OriginType } from '@constants';
+
+describe('ApixDatacenterInterceptor', () => {
+	const urlPrefix = 'url/';
+	const regionValue = 'usa';
+	const req: HttpRequest<any> = {
+		url: `${urlPrefix}${environment.datacenterStub}`,
+		body: { },
+		headers: new HttpHeaders(),
+		reportProgress: true,
+		withCredentials: false,
+		responseType: 'json',
+		method: '1',
+		params: new HttpParams(),
+		urlWithParams: 'url',
+		serializeBody: () => '',
+		detectContentTypeHeader: () => '',
+		clone: () => Object.assign({ }, req),
+	};
+
+	const next: HttpHandler = {
+		handle: () => new Observable<HttpEvent<any>>(),
+	};
+
+	let apixDatacenterInterceptor: ApixDatacenterInterceptor;
+	let apixIdentityService: ApixIdentityService;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [
+				HttpClientTestingModule,
+			],
+			providers: [
+				ApixDatacenterInterceptor,
+				{
+					provide: UserResolve,
+					useValue: {
+						getDataCenter: () => from([regionValue]),
+					},
+				},
+				{
+					provide: ApixIdentityService,
+					useValue: {
+						testOrigin: (_url: any) => OriginType.SDP,
+					},
+				},
+			],
+		});
+		apixDatacenterInterceptor = TestBed.get(ApixDatacenterInterceptor);
+		apixIdentityService = TestBed.get(ApixIdentityService);
+	});
+
+	it('should add datacenter value for SDP origin APIs', () => {
+		spyOn(req, 'clone');
+
+		const modifiedRequest = {
+			url: `${urlPrefix}${regionValue}`,
+		};
+
+		apixDatacenterInterceptor.intercept(req, next)
+		.subscribe();
+
+		expect(req.clone)
+			.toHaveBeenCalledWith(modifiedRequest);
+	});
+
+	it('should ignore an RMA APIx http request', () => {
+		apixIdentityService.testOrigin = (_url: any) => OriginType.RMA;
+
+		spyOn(next, 'handle')
+			.and
+			.callThrough();
+
+		apixDatacenterInterceptor.intercept(req, next)
+		.subscribe();
+
+		expect(next.handle)
+			.toHaveBeenCalledWith(req);
+	});
+});
---
+++ src/app/interceptors/apix-datacenter.interceptor.ts
@@ -0,0 +1,41 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserResolve } from '../utilities/user-resolve';
+import { mergeMap, take } from 'rxjs/operators';
+import { environment } from '@environment';
+import { ApixIdentityService } from './apix-identity.service';
+import { OriginType } from '@constants';
+
+export class ApixDatacenterInterceptor implements HttpInterceptor {
+
+	constructor (
+		private userResolve: UserResolve,
+		private apixIdentityService: ApixIdentityService,
+	) { }
+
+	/**
+	 * This interceptor is responsible to replace the region stub with proper region specific
+	 * text to construct region specific URLs (Only for SDP origin APIs)
+	 * @param req Original request object
+	 * @param next HTTP request handler
+	 * @returns http event next
+	 */
+	public intercept (req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		const url = new URL(req.url, environment.origin);
+
+		if (this.apixIdentityService.testOrigin(url) !== OriginType.SDP) {
+			return next.handle(req);
+		}
+
+		return this.userResolve.getDataCenter()
+		.pipe(
+			take(1),
+			mergeMap(dcLocation => {
+				const request = req.clone({
+					url: req.url.replace(environment.datacenterStub, dcLocation),
+				});
+				return next.handle(request);
+			}),
+		);
+	}
+}
