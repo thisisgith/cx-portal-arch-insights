47e05c13748925ebe5ee8235efdc4677fd283402
Peter Wang <petewan3@cisco.com>
2019-12-16 17:01:22 -0500

    fix(feedback): Add 'User Role', 'Cx Level' to the feedback email body. Closes CSCvs46811

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -8,6 +8,9 @@ import { EmailControllerService } from '@sdp-api';
 import { environment } from '@environment';
 import { I18n } from '@cisco-ngx/cui-utils';
 import * as _ from 'lodash-es';
+import { UserResolve } from '@utilities';
+import { forkJoin } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 /** Data Interface */
 interface Data {
@@ -30,26 +33,38 @@ export class FeedbackComponent implements OnInit {
 	public feedbackForm: FormGroup;
 	public isSubmitting = false;
 	public thumbSelected: 'up' | 'down' | '';
-
-	private user = this.profileService.getProfile();
-
 	public feedbackMaxLength = 32000;
 	public thumbValue = '-';
 	public okayToContact = 'No';
-	private userEmail = this.user.cpr.pf_auth_email;
+	private userProfile = this.profileService.getProfile();
+	private userEmail = this.userProfile.cpr.pf_auth_email;
 	public emailParams = {
 		from: this.userEmail,
 		subject: I18n.get('_FeedbackEmailSubject_'),
 		to: environment.feedbackToEmail,
 	};
 	public data: Data = { };
+	private cxLevel: number;
+	private userRole: string;
 
 	constructor (
 		private cuiModalService: CuiModalService,
 		private emailService: EmailControllerService,
 		private formBuilder: FormBuilder,
 		private profileService: ProfileService,
-	) { }
+		private userResolve: UserResolve,
+	) {
+		forkJoin(
+			this.userResolve.getCXLevel()
+			.pipe(take(1)),
+			this.userResolve.getRole()
+			.pipe(take(1)),
+		)
+		.subscribe(([cxLevel, userRole]) => {
+			this.cxLevel = cxLevel;
+			this.userRole = userRole;
+		});
+	}
 
 	/**
 	 * Onit
@@ -77,10 +92,9 @@ export class FeedbackComponent implements OnInit {
 	 */
 	private generateBody () {
 		// TODO: add the following to the email body
-		// Offer Level, Cisco team comtact, Smart Account/Virtual Account detail
-		// Solution selected, Use case selected
+		// Cisco team contact, Virtual Account detail
 
-		const cpr = _.get(this.user, 'cpr', { });
+		const cpr = _.get(this.userProfile, 'cpr', { });
 
 		if (this.feedbackForm.value.allowedContact) {
 			this.okayToContact = 'Yes';
@@ -94,6 +108,8 @@ export class FeedbackComponent implements OnInit {
 			`Access Level\n${_.get(cpr, 'pf_auth_user_level', 'N/A')}`,
 			`Organization Name\n${_.get(cpr, 'pf_auth_company_name', 'N/A')}`,
 			`Smart Account ID\n${_.get(this.data, 'saId', 'N/A')}`,
+			`User Role\n${this.userRole}`,
+			`CX Level\n${this.cxLevel}`,
 			`Success Track\n${_.get(this.data, 'solution', 'N/A')}`,
 			`UseCase\n${_.get(this.data, 'useCase', 'N/A')}`,
 			`Current Pitstop\n${_.get(this.data, 'pitstop', 'N/A')}`,
