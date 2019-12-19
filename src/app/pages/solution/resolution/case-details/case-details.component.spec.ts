561e724bcbcd93a10e903da7f8aae33376fef428
sruv <sruv@cisco.com>
2019-12-19 22:30:38 +0530

    tweak(prob-res/oc) : Length of files

--- src/app/pages/solution/resolution/case-details/case-details.component.spec.ts
+++ src/app/pages/solution/resolution/case-details/case-details.component.spec.ts
@@ -168,7 +168,8 @@ describe('CaseDetailsComponent', () => {
 	});
 
 	it('set number of files count from case details response', () => {
-		const caseDetails = { result: { response: { getBrokerResponse: { downloadInfo: { noOfFiles: 4, fileDetail: [{}, {}, {}] } } } } };
+		const caseDetails = { result: { response: { getBrokerResponse: { downloadInfo: { noOfFiles: 4,
+			fileDetail: [{ }, { }, { }] } } } } };
 		component.populateCaseFilesList(caseDetails);
 		expect(component.numberOfFiles)
 			.toEqual(3);
