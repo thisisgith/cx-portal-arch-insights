fe29095055af620f8ef92de9657a8cd964b70532
Mrinal <mchandu@cisco.com>
2020-01-29 11:56:56 +0530

    fix:Fixed suggestedAction and watchNow bugs

--- src/app/pages/solution/lifecycle/lifecycle.component.ts
+++ src/app/pages/solution/lifecycle/lifecycle.component.ts
@@ -1235,13 +1235,9 @@ export class LifecycleComponent implements OnDestroy {
 	 */
 	public resetFilter () {
 		this.resetSelectStatus();
-		const nextAction =  _.find(this.componentData.racetrack.pitstop.pitstopActions,
-			{ isComplete: false });
-		const actionName = nextAction ? nextAction.name : null;
-		if (this.componentData.params.suggestedAction !== actionName) {
-			this.componentData.params.suggestedAction = actionName;
-			this.loadLifecycleInfo();
-		}
+		this.componentData.params.suggestedAction = null;
+		this.loadLifecycleInfo();
+		
 	}
 
 	/**
@@ -2209,8 +2205,7 @@ export class LifecycleComponent implements OnDestroy {
 				stage,
 				actionsCompPercent: this.currentPitstopCompPert,
 			};
-			const nextAction = pitstop ? _.find(pitstop.pitstopActions, { isComplete: false }) : null;
-			this.componentData.params.suggestedAction = (this.enableCheckList && nextAction) ? nextAction.name : null;
+			this.componentData.params.suggestedAction = null;
 			if (pitstop) {
 				pitstop.pitstopActions.map(ptstopActn => {
 					ptstopActn.description = this.parseHtmlText(ptstopActn.description);
@@ -2520,7 +2515,12 @@ export class LifecycleComponent implements OnDestroy {
 
 		// cross launch if partner atx
 		if (atx.providerInfo) {
-			this.crossLaunch(event, atx.recordingURL);
+			if(atx.recordingURL.includes("https://")){
+				this.crossLaunch(event, decodeURIComponent(atx.recordingURL));
+			}
+			else{
+				this.crossLaunch(event, "https://"+decodeURIComponent(atx.recordingURL))
+			}
 			return;
 		}
 		const atxWatchData = {
